f65704ef09a2eb0b2dc29959a9e389f336d33fe2
zuenamash <mnachariazuenah@gmail.com>
2022-04-06 21:19:28 +0300

    first commit

---
+++ Madlibs.py
@@ -0,0 +1,33 @@
+
+color = input("Enter a color : ")
+plural_noun = input("Enter a Plural noun :")
+celebrity = input("Enter a celebrity:")
+language = input("Enter a language :")
+Trainer = input("Enter a Trainer :")
+goal = input("Enter goal :")
+classmate = input("Enter classmate :")
+deskmate = input("Enter deskmate  :")
+neighbours =input("Enter neighbours :")
+gender = input("Enter gender :")
+country =input ("Enter country :")
+culture = input("Enter culture :")
+
+
+
+print("Roses are + color")
+print("plural noun + are blue")
+print("I love + celebrity")
+
+print("Python is language")
+print("Mwai is Trainer")
+print("Workhard goal")
+
+print("Yugi is my classmate")
+print("Kiki too is my deskmate")
+print("Kaki  and Kaki are neighbours")
+print("all are girls gender")
+print("Also they come from Kenya country")
+print("The maasai are the best culture")
+
+
+
